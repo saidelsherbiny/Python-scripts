f4067446ecc6407f7052e3e35f6205e8d25fe5f1
Said El-Sherbiny <79178690+saidelsherbiny@users.noreply.github.com>
2024-01-03 13:30:17 -0500

    Update Subnet Mask Calculator.py

--- Subnet Mask Calculator.py
+++ Subnet Mask Calculator.py
@@ -6,17 +6,10 @@ with open('ips.csv') as file:
     count = 0
     print("computers not in the subnet:")
     for row in reader:
-        if (ipaddress.ip_address(row[0]) in ipaddress.ip_network('10.1.0.0/16')):
+        if (ipaddress.ip_address(row[0]) in ipaddress.ip_network('0.xxx.x.x/xx')):
             print ("",end = "") 
-        elif (ipaddress.ip_address(row[0]) in ipaddress.ip_network('10.129.0.0/13',False)):
+        elif (ipaddress.ip_address(row[0]) in ipaddress.ip_network('10.xxx.x.x/xx',False)):
             print ("",end = "")
-        elif (ipaddress.ip_address(row[0]) in ipaddress.ip_network('10.128.128.0/22')):
-            print ("",end = "")
-        elif (ipaddress.ip_address(row[0]) in ipaddress.ip_network('10.128.0.0/24')):
-            print ("",end = "")
-        elif (ipaddress.ip_address(row[0]) in ipaddress.ip_network('10.6.0.0/16')):
-            print ("",end = "")
-        else:
             print(row[0])
         count=count+1
     print(count)
